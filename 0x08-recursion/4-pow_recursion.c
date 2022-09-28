241080d6df451ae7661315d44d5c2e4ea4c11b97
Okalll <vivieokall@gmail.com>
2022-09-28 05:50:47 -0700

    Correction

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _pow_recursion - returns value of x raised to the pow of y
+ * _pow_recursion - returns the value of x  to the power of y
  * @x: the number
  * @y: the power
  * Returns: x to the power of y
