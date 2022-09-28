586b1fd8f38fad04d5919d9ef3ea05fbc49d71e4
Okalll <vivieokall@gmail.com>
2022-09-28 06:13:36 -0700

    Prime numbers

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * is_prime_number - finds prime numbers among integers
+ * @n: the integer
+ * Returns: 0 or 1
+ */
+int is_prime_number(int n)
+{
+	if (n = prime number)
+		return (1);
+	else
+		return (0);
+}
