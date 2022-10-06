9dff5a1fb531cc3675980fce7512833937451110
Okalll <vivieokall@gmail.com>
2022-10-06 06:50:40 -0700

    Task5

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
