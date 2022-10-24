f6b93a76b3776110501303814cbacb93c57d4ea3
Okalll <vivieokall@gmail.com>
2022-10-24 08:32:59 -0700

    Final touches

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,13 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 /**
- * main - check the code
+ * pbMain - for management of list
  *
  * Return: Always 0
  */
 
-int main(void)
+int pbMain(void) __attribute__ ((constructor)); int pbMain(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
